4b1a805fc6a6057ef49af2d8aab8edf6c1e8b8dd
Gautam Sharma <gautam_sharma1@yahoo.com>
2023-03-20 00:19:56 +0530

    modified contestFolder.sh

--- contestFolder.sh
+++ contestFolder.sh
@@ -9,7 +9,7 @@ p1='A'
 for(( i=0 ; i<$noProblems ; i++));
 do
     touch $p1.cpp
-    cp /Users/gautamsharma/Desktop/Ultimate_Push/currentTemplate.cpp $p1.cpp
+    cp /Users/gautamsharma/Desktop/Ultimate_Push/usefulCodes/Templates/currentTemplate.cpp $p1.cpp
     mv $p1.cpp /Users/gautamsharma/Desktop/Ultimate_Push/$contestName
     p1=$(echo "$p1" | tr "0-9A-Z" "1-9A-Z_")
 done
